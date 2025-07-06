b33791cbbaed0921be036db347e8b15a1c99b77f
Arcoti <ArchdukeArcoti@gmail.com>
2025-07-06 13:54:59 +0800

    Added function to test model

--- src/neural_network/test.py
+++ src/neural_network/test.py
@@ -1,4 +1,19 @@
+import numpy as np
+
+from .solve import solve
+
+# Y here is simply the labels
 def test(X, Y, parameters):
-    ...
-    # Solve then calculate accuracy score
-    # Number of correct / total number of predictions
+    # Solve
+    results = solve(X, parameters)
+
+    # Calculate Number of Correct Predictions
+    results_int = results.astype(int)
+    label_int = Y.astype(int)
+    total_correct = np.sum((results_int == label_int).astype(int))
+
+    # Calculate Accuracy Score
+    total_predictions = results.shape[0]
+    accuracy_score = total_correct / total_predictions
+
+    return accuracy_score
