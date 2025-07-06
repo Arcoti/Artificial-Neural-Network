9c99da8e7f015bbed851a20677373281f1ef2f74
Arcoti <ArchdukeArcoti@gmail.com>
2025-07-06 10:24:05 +0800

    Added function to load mnist dataset

---
+++ src/dataset/mnist_dataset.py
@@ -0,0 +1,10 @@
+import tensorflow_datasets as tfds
+
+def retrieve_mnist_dataset():
+    ds_train_raw = tfds.load('mnist', split='train', shuffle_files=True, as_supervised=True)
+    ds_test_raw = tfds.load('mnist', split='test', shuffle_files=True, as_supervised=True)
+
+    ds_train = tfds.as_numpy(ds_train_raw)
+    ds_test = tfds.as_numpy(ds_test_raw)
+
+    return ds_train, ds_test
