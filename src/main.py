ed88dfd306dd5e5dc0a5cd13a5e0d36fade167df
Arcoti <ArchdukeArcoti@gmail.com>
2025-07-12 13:34:43 +0800

    fix the main function

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 from .neural_network import *
 from .dataset import *
 
+LABEL_SIZE = 10
+BATCH_SIZE = 32
+EPOCHS = 2
+LEARNING_RATE = 0.1
+DIMENSIONS = [784, 128, 64, 10]
+
 def main():
     ds_train, ds_test = retrieve_mnist()
-    x_train, y_train = clean(ds_train, as_label=False)
-    x_test, y_test = clean(ds_test, as_label=True)
+    x_train, y_train = clean_train(ds_train, BATCH_SIZE, LABEL_SIZE)
+    x_test, y_test = clean_test(ds_test, BATCH_SIZE)
 
-    parameters, cost_history = train(x_train, y_train, [784, 128, 64, 10], 2, 0.1, 32)
+    parameters, cost_history = train(x_train, y_train, DIMENSIONS, EPOCHS, LEARNING_RATE)
     print(parameters)
-    # accuracy_score = test(x_test, y_test, parameters)
-    # print(accuracy_score)
+
+    accuracy_score = test(x_test, y_test, parameters)
+    print(accuracy_score)
 
 if __name__ == "__main__":
     main()
