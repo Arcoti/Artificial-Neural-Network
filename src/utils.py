0988358e9b55efcb15ec2e3c8b9079502b2ed408
Arcoti <ArchdukeArcoti@gmail.com>
2025-07-13 18:59:36 +0800

    Added learning rate finder

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def learning_rate_finder(step: int, total: int, start=1e-7, end=1):
+    return start * (end / start) ** (step / (total - 1))
+
+def plot(x, y, x_label: str, y_label: str, title: str):
+    plt.figure(figsize=(8, 5))
+    plt.plot(x, y)
+
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.show()
