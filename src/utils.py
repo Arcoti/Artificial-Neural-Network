7867cc075976a212e2657acb19c455c7072c0461
Arcoti <ArchdukeArcoti@gmail.com>
2025-07-13 19:05:23 +0800

    Change starting for learning rate finder

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def learning_rate_finder(step: int, total: int, start=1e-7, end=1):
+def learning_rate_finder(step: int, total: int, start=1e-3, end=1):
     return start * (end / start) ** (step / (total - 1))
 
 def plot(x, y, x_label: str, y_label: str, title: str):
